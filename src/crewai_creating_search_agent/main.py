140c24952bc9796c3b5034172501f1eba95e9f25
Hari Thapliyaal <hari@dasarpai.com>
2025-01-08 22:14:25 +0530

    Update main.py

--- src/crewai_creating_search_agent/main.py
+++ src/crewai_creating_search_agent/main.py
@@ -60,6 +60,6 @@ def test():
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
 
-if __name__ == "__main__":
-    print("Starting main execution...")
-    run()
+# if __name__ == "__main__":
+#     print("Starting main execution...")
+#     run()
